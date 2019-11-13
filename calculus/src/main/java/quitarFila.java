e23aaf15f81fc4a98941e8bac88e460b0e0764b4
martimartm <49487924+martimartm@users.noreply.github.com>
2019-11-13 19:02:15 +0100

    Create quitarFila.java

---
+++ calculus/src/main/java/quitarFila.java
@@ -0,0 +1,23 @@
+public static double [][] quitarFila (double [][] m, int n) { 
+ 
+    // if(!esMatriz(m)){   
+    //                    return null;  }     
+
+  if (n >= m.length  ||  n<0){  
+        return  null;   
+                             }   
+
+   double [][] res  = new  double[m.length‐1][m[0].length];
+
+     for(int  i = 0;i <  res.length;i++){      
+       for(int   j =  0; j < res[i].length;j++) {  
+            if  (i <  n){            
+                res[i][j]  =  m[i][j];            
+                        }                   
+             else if  (i >=n && i+1 <  m.length){   
+                         res[i][j] = m[i+1][j];            
+                                                }                     
+                                                }                
+                                        }                 
+             return res;
+                                                             }    
